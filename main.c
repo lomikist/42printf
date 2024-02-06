459d3a94f736e23c989563246357208a51b32b7c
arman <armansarg01@gmail.com>
2024-02-05 21:13:10 -0800

    sting

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <unistd.h>
-int ft_printf(const char *str, ...);
+
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
@@ -10,144 +10,67 @@ void	ft_putchar(char c)
 
 void	ft_putstr(char *str)
 {
-	int	i;
+	int i;
 
 	i = 0;
-	while (str[i] != 0)
+	while (str[i])
 	{
-		write(1, &str[i], 1);
+		ft_putchar(str[i]);
 		i++;
 	}
 }
+
 void	ft_putnbr(int nb)
 {
-	char	k;
+	unsigned int	nbr;
 
-	if (nb == -2147483648)
-		write(1, "-2147483648", 11);
-	else
+	if (nb < 0)
 	{
-		if (nb < 0)
-		{
-			write(1, "-", 1);
-			nb = -nb;
-		}
-		if (nb > 9)
-			ft_putnbr(nb / 10);
-		k = nb % 10 + '0';
-		write (1, &k, 1);
+		ft_putchar('-');
+		nbr = nb * -1;
 	}
-}
-
-static int	int_size(long long int n)
-{
-	int	len;
-
-	len = 0;
-	if (n < 1)
-		len++;
-	while (n)
-	{
-		n /= 10;
-		len++;
-	}
-	return (len);
-}
-
-static char	*new_str(size_t n)
-{
-	char	*str;
-
-	str = (char *)malloc(n + 1);
-	if (!str)
-		return (NULL);
-	return (str);
-}
-
-char	*ft_itoa(int n)
-{
-	unsigned int	m;
-	int				sign;
-	int				len;
-	char			*str;
-
-	sign = 0;
-	if (n < 0)
-		sign = 1;
-	len = int_size(n);
-	str = new_str(len);
-	if (!str)
-		return (NULL);
-	*(str + len) = '\0';
-	if (n < 0)
-		m = -n;
 	else
-		m = n;
-	while (len--)
+		nbr = nb;
+	if (nbr >= 10)
+		ft_putnbr(nbr / 10);
+	ft_putchar(nbr % 10 + 48);
+}
+
+void decToHex(long long int decimal) 
+{
+	char	hexadecimal[100];
+	int		indx = 0;
+
+	if (decimal == 0) 
+		return;
+	while (decimal > 0) 
 	{
-		*(str + len) = 48 + m % 10;
-		m /= 10;
+		int remainder = decimal % 16;
+
+		if (remainder < 10)
+			hexadecimal[indx++] = remainder + '0';
+		else
+			hexadecimal[indx++] = remainder + 'a' - 10;
+		decimal /= 16;
 	}
-	if (sign)
-		*(str) = 45;
-	return (str);
-}
-
-unsigned int	ft_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i] != 0)
-		i++;
-	return (i);
-}
-
-void	printHex(long int decimal, int flag, int is_pointer) 
-{
-    char	hexadecimal[100];
-    int		index;
-	int		remainder;
-	char	hex_char;
-
-	if (flag)
-		hex_char = 'a';
-	else
-		hex_char = 'A';
-	index = 0;
-    if (decimal == 0) 
-        return ;
-    while (decimal > 0) 
-    {
-        remainder = decimal % 16;
-        if (remainder < 10)
-            hexadecimal[index++] = remainder + '0';
-        else
-            hexadecimal[index++] = remainder + hex_char - 10;
-        decimal /= 16;
-    }
-	if (is_pointer)
-		write(1, "0x", 2);
-	while (index >= 0)
-		write(1, &hexadecimal[index--], 1);
+	write(1, "0x", 2);
+	while (indx-- >= 0)
+		write(1, &hexadecimal[indx], 1);
 }
 
 static char	*detect_next_char(char *str, va_list args)
 {
-	long long int value = va_arg(args, long long int);
+	long long int	value;
 
+	value = va_arg(args, long long int);
 	if (*str == 'd')
 		ft_putnbr(value);
 	else if (*str == 'c')
 		ft_putchar(value);
+	else if (*str == 'x')
+		decToHex(value);
 	else if (*str == 'p')
-		printHex(value, 1, 1);
-	else if (*str == 'x') 
-		printHex(value, 1, 0);
-	else if (*str == 'X')
-		printHex(value, 0, 0);
-	else if (*str == '%')
-		ft_putchar('%');
+		decToHex(value);
 	else if (*str == 's')
 		ft_putstr((char *)value);
 	// else if (*str == 'i')
@@ -156,7 +79,6 @@ static char	*detect_next_char(char *str, va_list args)
 	// else if (*str == 'x')
 	// else if (*str == 'X')
 	// else if (*str == '%')
-	// write(1, number_str, ft_strlen(number_str));
 	return str;
 }
 
@@ -164,28 +86,21 @@ int ft_printf(const char *str, ...)
 {
 	va_list args;
 	va_start(args, str);
-	while (*str != '\0')
+	while (*str != '\0')// str + 1
 	{
 		if (*str == '%')
 			str = detect_next_char((char *)(str + 1), args);
 		else
-			write(1, str, 1);
+			write(1, str, 1);		
 		str++;
 	}
 	return 1;
 }
 
+#include <stdio.h>
 int	main()
 {
-	// char *string = "hell%od";
-	// ft_printf("-%d- -%c- -%d- -%p-", 5443, 's', 5, );
-	char *somestr = "hello darkness my old friend";
-	int a = 123;
-	int *pa = &a;
-	// long long int pval = pa;?
-	// char k = 'l';
-	printf("-%d- -%p- -%x- -%X- -%%- -%s- \n", a, pa, a, a, somestr);
-	ft_printf("-%d- -%p- -%x- -%X- -%%- -%s-\n", a, pa, a, a, somestr);
-	// printf("-%d-\n", pa);
-	// printHex(pa);
+	char *string = "hellsdad";
+	ft_printf("-%d-  -%d-  -%x-  -%s-\n", 5443, 's', 124, &string[0]);
+	printf("-%d-  -%d-  -%x-  -%s-\n", 5443, 's', 124, &string[0]);
 }
